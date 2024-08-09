712fcbf3d165e420a27c509bc96a4c4a8417f01b
n4days <alirsyhad.ramadhan49@gmail.com>
2024-08-09 11:21:57 +0700

    feat(ujian) add controller Mahasiswa

--- app/src/main/java/com/example/simkampus/Mahasiswa.java
+++ app/src/main/java/com/example/simkampus/Mahasiswa.java
@@ -1,14 +1,29 @@
 package com.example.simkampus;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 public class Mahasiswa extends AppCompatActivity {
 
+    ActionBar actionBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mahasiswa);
+
+        actionBar = getSupportActionBar();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
