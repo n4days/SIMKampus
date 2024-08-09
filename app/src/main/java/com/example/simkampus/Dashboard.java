42904851ec618746bacd423aadeaea01ee76273b
n4days <alirsyhad.ramadhan49@gmail.com>
2024-08-09 10:25:05 +0700

    feat(ujian) add controller Dashboard

--- app/src/main/java/com/example/simkampus/Dashboard.java
+++ app/src/main/java/com/example/simkampus/Dashboard.java
@@ -2,13 +2,30 @@ package com.example.simkampus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Dashboard extends AppCompatActivity {
+
+    Button btnViewData, btnInputData, btnInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        btnViewData = findViewById(R.id.btnViewData);
+        btnInputData = findViewById(R.id.btnInputData);
+        btnInfo = findViewById(R.id.btnInfo);
+
+        btnViewData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Dashboard.this, DataMahasiswa.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/simkampus/DataMahasiswa.java
@@ -0,0 +1,14 @@
+package com.example.simkampus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DataMahasiswa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_mahasiswa);
+    }
+}
