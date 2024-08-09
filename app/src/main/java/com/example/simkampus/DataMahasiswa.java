fef2b799458223149c35e403119e64b918595731
n4days <alirsyhad.ramadhan49@gmail.com>
2024-08-09 11:18:55 +0700

    feat(ujian) add controller Data Mahasiswa and ui Mahasiswa

--- app/src/main/java/com/example/simkampus/DataMahasiswa.java
+++ app/src/main/java/com/example/simkampus/DataMahasiswa.java
@@ -3,19 +3,36 @@ package com.example.simkampus;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class DataMahasiswa extends AppCompatActivity {
 
     ActionBar actionBar;
+
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_mahasiswa);
 
+        fab = findViewById(R.id.fab);
+
         actionBar = getSupportActionBar();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DataMahasiswa.this, Mahasiswa.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
