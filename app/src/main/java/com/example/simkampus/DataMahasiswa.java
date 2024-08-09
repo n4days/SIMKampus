05a68dd30af348e90378d6892656362e95458641
n4days <alirsyhad.ramadhan49@gmail.com>
2024-08-09 10:47:03 +0700

    feat(ujian) add action back and ui Data Mahasiswa

--- app/src/main/java/com/example/simkampus/DataMahasiswa.java
+++ app/src/main/java/com/example/simkampus/DataMahasiswa.java
@@ -1,14 +1,31 @@
 package com.example.simkampus;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 public class DataMahasiswa extends AppCompatActivity {
 
+    ActionBar actionBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_mahasiswa);
+
+        actionBar = getSupportActionBar();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
