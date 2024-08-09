31af9e127c4f5f6a2638c168f2d2ae157ef8b556
n4days <alirsyhad.ramadhan49@gmail.com>
2024-08-09 11:53:11 +0700

    feat(ujian) add controller Mahasiswa AddData

--- app/src/main/java/com/example/simkampus/DatabaseHelper.java
+++ app/src/main/java/com/example/simkampus/DatabaseHelper.java
@@ -14,16 +14,26 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String COL_2 = "EMAIL";
     public static final String COL_3 = "PASSWORD";
 
+    private static final String TABLE_NAME_2 = "mahasiswa_table";
+    private static final String COLUMN_ID = "ID";
+    private static final String COLUMN_NIM = "NIM";
+    private static final String COLUMN_NAME = "NAMA";
+    private static final String COLUMN_DOB = "DOB";
+    private static final String COLUMN_GENDER = "GENDER";
+    private static final String COLUMN_ADDRESS = "ADDRESS";
+
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
     }
 
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, EMAIL TEXT, PASSWORD TEXT)");
+        db.execSQL("create table " + TABLE_NAME_2 + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NIM TEXT, NAMA TEXT, DOB TEXT, GENDER TEXT, ADDRESS TEXT)");
     }
 
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_2);
         onCreate(db);
     }
 
@@ -48,6 +58,19 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE EMAIL = ? AND PASSWORD = ?", new String[]{email, password});
         if (cursor.getCount() > 0) return true;
         else return false;
+    }
+
+    public long addData(InfoMahasiswa data) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NIM, data.getNIM());
+        values.put(COLUMN_NAME, data.getNAMA());
+        values.put(COLUMN_DOB, data.getDOB());
+        values.put(COLUMN_GENDER, data.getGENDER());
+        values.put(COLUMN_ADDRESS, data.getADDRESS());
+        long id = db.insert(TABLE_NAME_2, null, values);
+        db.close();
+        return id;
     }
 
 }
--- app/src/main/java/com/example/simkampus/Mahasiswa.java
+++ app/src/main/java/com/example/simkampus/Mahasiswa.java
@@ -4,18 +4,71 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class Mahasiswa extends AppCompatActivity {
 
     ActionBar actionBar;
+
+    private EditText etNomor, etName, etDate, etGender, etAddress;
+    private Button btnSave;
+
+    private DatabaseHelper dbHelper;
+    private InfoMahasiswa infoMahasiswa;
+    private boolean isEdit = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mahasiswa);
 
+        etNomor = findViewById(R.id.etNomor);
+        etName = findViewById(R.id.etName);
+        etDate = findViewById(R.id.etDate);
+        etGender = findViewById(R.id.etGender);
+        etAddress = findViewById(R.id.etAddress);
+        btnSave = findViewById(R.id.btnSave);
+        dbHelper = new DatabaseHelper(this);
+
         actionBar = getSupportActionBar();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nim = etNomor.getText().toString().trim();
+                String name = etName.getText().toString().trim();
+                String date = etDate.getText().toString().trim();
+                String gender = etGender.getText().toString().trim();
+                String address = etAddress.getText().toString().trim();
+                if (nim.isEmpty() || nim.isEmpty()) {
+                    Toast.makeText(Mahasiswa.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (isEdit) {
+//                    note.setTitle(title);
+//                    note.setContent(content);
+//                    note.setDate(date);
+//                    dbHelper.updateNote(note);
+                } else {
+                    infoMahasiswa = new InfoMahasiswa();
+                    infoMahasiswa.setNIM(nim);
+                    infoMahasiswa.setNAMA(name);
+                    infoMahasiswa.setDOB(date);
+                    infoMahasiswa.setGENDER(gender);
+                    infoMahasiswa.setADDRESS(address);
+                    dbHelper.addData(infoMahasiswa);
+                }
+                finish();
+            }
+        });
     }
 
     public boolean onSupportNavigateUp() {
